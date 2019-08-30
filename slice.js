b2ca95da52fb4180d12cf78156b20c59c4e81e4b
endy <endyaduba@gmail.com>
2019-08-30 01:00:03 +0100

    slice()

---
+++ slice.js
@@ -0,0 +1,8 @@
+// Basic Data Structures: Copy Array Items Using slice()
+// The next method we will cover is slice(). slice(), rather than modifying an array, copies, or extracts, a given number of elements to a new array, leaving the array it is called upon untouched. slice() takes only 2 parameters — the first is the index at which to begin extraction, and the second is the index at which to stop extraction (extraction will occur up to, but not including the element at this index). Consider this:
+
+let weatherConditions = ['rain', 'snow', 'sleet', 'hail', 'clear'];
+
+let todaysWeather = weatherConditions.slice(1, 3);
+// todaysWeather equals ['snow', 'sleet'];
+// weatherConditions still equals ['rain', 'snow', 'sleet', 'hail', 'clear']
